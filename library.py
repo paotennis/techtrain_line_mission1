ea11060d3f6140cc4712d715b3c1fc9548c0ac59
paotennis <trust990309@yahoo.co.jp>
2021-04-22 14:00:38 +0900

    general framework

--- library.py
+++ library.py
@@ -1,7 +1,12 @@
+import re
+
 def function_input():
     st = input()
     if st == '': #blank line
         return 0,10000
+    if re.search('^\d{2}:\d{2}:\d{2}.\d{3}',st) == None:
+        print("-3")
+        exit(-3)
     else:
         s = st.split() #s[0]:hh:mm:ss.fff  s[1]:distance(m)
         hour, minute, second = s[0].split(":")
@@ -9,19 +14,38 @@ def function_input():
         times = int(hour) * 3600000 + int(minute) * 60000 + int(second) * 1000 +int(decimal) #times[ms]
         temp = float(s[1])*10
         distance = int(temp) #distance[10cm]
+        if distance >= 1000:
+            print("-6")
+            exit(-6)
         return times,distance
 
 def payment(first_pay,low_times,add_distance,add_night_distance,add_low_times):
-    low_pay = (low_times + add_low_times * 1.25) // 90000 #count low speed 
-    add_pay = (add_distance + add_night_distance * 1.25) // 2370 #count add
+    low_pay = low_times + add_low_times #count low speed 
+    add_pay = (add_distance // 2370) + (add_night_distance * 1.25) // 2370 #count add
     if (add_distance + add_night_distance * 1.25) % 2370 != 0:
         add_pay+=1
     credit = first_pay + (low_pay + add_pay) * 80 #sum credit
-    print(low_pay)
-    print(add_pay)
     print(credit)
     return 0
 
-def countdistance(first_distance,before_distance,distance):
-    dist = distance - before_distance
+def countdistance(before_distance,distance):
+    dist = distance - before_distance #distance calculation
     return dist
+
+def countlowtime(before_distance,distance,before_times,times):
+    dist = distance - before_distance #distance calculation
+    tim = times - before_times #time calculation
+    speed = ((dist * 100 / tim)*3.6) # speed calculation [km/h]
+    if speed <= 10.0:
+        return tim // 90000
+    else:
+        return 0
+
+def countnightlowtime(before_distance,distance,before_times,times):
+    dist = distance - before_distance
+    tim = times - before_times
+    speed = ((dist * 100 / tim)*3.6)
+    if speed <= 10.0:
+        return tim * 1.25 // 90000
+    else:
+        return 0
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import library
 
 first_pay = 410
-low_times = 0
+add_low_times = 0
 add_distance = 0
 add_night_distance = 0
-add_low_times = 0
+add_night_low_times = 0
 
 
 if __name__ == "__main__":
@@ -14,16 +14,32 @@ if __name__ == "__main__":
         times,distance=library.function_input()
         if distance >= 10000:
             if count >= 3:
+                if add_distance == 0:
+                    print("-5")
+                    exit(-5)
                 add_distance -= 10520
                 if add_distance < 0:
                     add_distance = 0
-                library.payment(first_pay,low_times,add_distance,add_night_distance,add_low_times)
+                library.payment(first_pay,add_low_times,add_distance,add_night_distance,add_night_low_times)
                 exit(0)
             else:
+                print("-1")
                 exit(-1)
         if count == 1:
             before_times = times; before_distance = distance
-            first_times = times; first_distance = distance
+            if distance != 0:
+                print("-4")
+                exit(-4)
         else:
-            add_distance += library.countdistance(first_distance,before_distance,distance)
-        print(times,distance)
+            if times - before_times <= 0:
+                print("-2")
+                exit(-2)
+            before_hour = before_times // 3600000; times_hour = times // 3600000
+            if before_hour < 5 or 22 <= before_hour < 29 or 46 <= before_hour < 53 or 70 <= before_hour < 77 or 94 <= before_hour:
+                if times_hour < 5 or 22 <= times_hour < 29 or 46 <= times_hour < 53 or 70 <= times_hour < 77 or 94 <= times_hour:
+                    add_night_distance += library.countdistance(before_distance,distance)
+                    add_night_low_times += library.countnightlowtime(before_distance,distance,before_times,times)
+                    continue
+            add_distance += library.countdistance(before_distance,distance)
+            add_low_times += library.countlowtime(before_distance,distance,before_times,times)
+            before_times = times; before_distance = distance
